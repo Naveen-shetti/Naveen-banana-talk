3bf750e018d7ff636a20485b01f9832113a28be1
Naveen-shetti <125954752+Naveen-shetti@users.noreply.github.com>
2023-02-26 14:54:27 +0530

    updated

--- app.js
+++ app.js
@@ -2,8 +2,21 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
+var serverURL = "https://api.funtranslations.com/translate/dothraki.json";
+
+function getTranslatedURL(input) {
+  return serverURL + "?" + "text=" + input;
+}
+
 function clickHandler() {
-  outputDiv.innerText = "your text is converted " + txtInput.value;
+  var inputTxt = txtInput.value;
+
+  fetch(getTranslatedURL(inputTxt))
+    .then((Response) => Response.json())
+    .then((json) => {
+      var translatedTxt = json.contents.translated;
+      outputDiv.innerText = translatedTxt;
+    });
 }
 
 btnTranslate.addEventListener("click", clickHandler);
